52433d3096c104bd467b3331e34f636b8f24f6b2
sphuakpong <cloneltorrer@gmail.com>
2024-07-08 11:07:38 +0700

    Change object geometry from BoxGeometry to SphereGeometry

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
+const geometry = new THREE.SphereGeometry(1, 32, 16);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
