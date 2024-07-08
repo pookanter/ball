d7a1fe3d83b0d6038997c5773255fcacfa3d0483
sphuakpong <cloneltorrer@gmail.com>
2024-07-08 15:53:54 +0700

    Add GUI controls for tweaking object positions and colors

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import './style.css'
 import * as THREE from 'three'
+import GUI from 'lil-gui'
+
+// Debug
+const gui = new GUI()
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -11,18 +15,36 @@ const scene = new THREE.Scene()
 const group = new THREE.Group()
 scene.add(group)
 
+const groupTweak = gui.addFolder('group')
+groupTweak.add(group.position, 'x').min(-3).max(3).step(0.01).name('x')
+groupTweak.add(group.position, 'y').min(-3).max(3).step(0.01).name('y')
+groupTweak.add(group.position, 'z').min(-3).max(3).step(0.01).name('z')
+
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(0.5, 32, 16),
     new THREE.MeshBasicMaterial({ color: 0xff0000 })
 )
+group.add(sphere)
 sphere.position.y = 0.75
 
-const base = new THREE.Mesh(
+const sphereTweak = gui.addFolder('sphere')
+sphereTweak.add(sphere.position, 'x').min(-3).max(3).step(0.01).name('x')
+sphereTweak.add(sphere.position, 'y').min(-3).max(3).step(0.01).name('y')
+sphereTweak.add(sphere.position, 'z').min(-3).max(3).step(0.01).name('z')
+sphereTweak.addColor(sphere.material, 'color').name('color')
+
+const box = new THREE.Mesh(
     new THREE.BoxGeometry(2, 0.4, 2),
     new THREE.MeshBasicMaterial({ color: 0x000fff3b })
 )
-group.add(sphere)
-group.add(base)
+group.add(box)
+
+const boxTweak = gui.addFolder('box')
+boxTweak.add(box.position, 'x').min(-3).max(3).step(0.01).name('x')
+boxTweak.add(box.position, 'y').min(-3).max(3).step(0.01).name('y')
+boxTweak.add(box.position, 'z').min(-3).max(3).step(0.01).name('z')
+boxTweak.addColor(box.material, 'color').name('color')
+
 // Axes helper
 const axesHelper = new THREE.AxesHelper(100)
 scene.add(axesHelper)
@@ -49,6 +71,9 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+gui.onChange(() => {
+    renderer.render(scene, camera)
+})
 
 // window.addEventListener('resize', () => {
 //     console.log('window resize')
